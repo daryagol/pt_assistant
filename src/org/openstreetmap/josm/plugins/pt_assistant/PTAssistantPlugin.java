39ec4f8efbcdb30b48e5d9ac13e6874a61901ade
daryagol <darya0705@gmail.com>
2016-05-22 12:42:41 +0200

    initial project version

---
+++ src/org/openstreetmap/josm/plugins/pt_assistant/PTAssistantPlugin.java
@@ -0,0 +1,90 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.pt_assistant;
+
+import javax.swing.ImageIcon;
+import javax.swing.JMenu;
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.gui.MapFrame;
+import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+import org.openstreetmap.josm.tools.ImageProvider;
+
+/**
+ * This is the main class of the PTAssistant plugin.
+ *
+ * @author daryagol / Darya Golovko
+ */
+public class PTAssistantPlugin extends Plugin {
+
+//	public static final ImageIcon ICON24 = new ImageProvider("icon24.png").get();
+//
+//	public String test;
+//some variables
+//	
+//	/**
+//	 * OS route separator
+//	 */
+//	public static final String SEPARATOR = System.getProperty("file.separator");
+
+	/**
+	 * Main constructor.
+	 *
+	 * @param info
+	 *            Required information of the plugin. Obtained from the jar
+	 *            file.
+	 */
+	public PTAssistantPlugin(PluginInformation info) {
+		super(info);
+
+		
+//		JOptionPane.showMessageDialog(null, "Download completed", "Question",
+//		        JOptionPane.INFORMATION_MESSAGE);
+
+//		createGridManuallyAction = new CreateGridManuallyAction(this);
+//		createGridManuallyMenu = MainMenu.add(Main.main.menu.viewMenu, createGridManuallyAction, false);
+//
+//		createGridAutomaticallyAction = new CreateGridAutomaticallyAction(this);
+//		createGridAutomaticallyMenu = MainMenu.add(Main.main.menu.viewMenu, createGridAutomaticallyAction, false);
+//
+//		splitGridAction = new SplitGridAction();
+//		splitGridMenu = MainMenu.add(Main.main.menu.viewMenu, splitGridAction, false);
+//		
+//		JMenu markGridMenu = new JMenu("Mark grid as");
+//		Main.main.menu.viewMenu.add(markGridMenu);
+//		this.markAsTodoAction = new MarkAsTodoAction();
+//		this.markAsTodoMenu = MainMenu.add(this.markGridMenu, this.markAsTodoAction);
+//		this.markAsDoingAction = new MarkAsDoingAction();
+//		this.markAsDoingMenu = MainMenu.add(this.markGridMenu, this.markAsDoingAction);
+//		this.markAsDoneAction = new MarkAsDoneAction();
+//		this.markAsDoneMenu = MainMenu.add(this.markGridMenu, this.markAsDoneAction);
+
+	}
+
+//	@Override
+//	public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {
+//		// does nothing
+//	}
+//	
+//	@Override
+//	public PreferenceSetting getPreferenceSetting() {
+//		// TODO
+//		return null;
+//	}
+//
+//	/**
+//	 * Returns a ImageProvider for the given string or null if in headless mode.
+//	 *
+//	 * @param s
+//	 *            The name of the file where the picture is.
+//	 * @return A ImageProvider object for the given string or null if in
+//	 *         headless mode.
+//	 */
+//	public static ImageProvider getProvider(String s) {
+//		if (Main.main == null) {
+//			return null;
+//		}
+//		return new ImageProvider(s);
+//	}
+}
