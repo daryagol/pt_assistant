50f7d71de28db70c50435e2022fe505663c2c0bb
daryagol <darya0705@gmail.com>
2016-05-25 00:42:56 +0200

    First PT test (PlatformsFirst) can be called from the validator

--- src/org/openstreetmap/josm/plugins/pt_assistant/PTAssistantPlugin.java
+++ src/org/openstreetmap/josm/plugins/pt_assistant/PTAssistantPlugin.java
@@ -1,32 +1,20 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.pt_assistant;
 
-import javax.swing.ImageIcon;
-import javax.swing.JMenu;
+import javax.swing.JOptionPane;
 
-import org.openstreetmap.josm.Main;
-import org.openstreetmap.josm.gui.MapFrame;
-import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
+import org.openstreetmap.josm.data.validation.OsmValidator;
+import org.openstreetmap.josm.data.validation.Test;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
-import org.openstreetmap.josm.tools.ImageProvider;
 
 /**
  * This is the main class of the PTAssistant plugin.
  *
  * @author daryagol / Darya Golovko
+ * @param <PlatformsFirst>
  */
 public class PTAssistantPlugin extends Plugin {
-
-//	public static final ImageIcon ICON24 = new ImageProvider("icon24.png").get();
-//
-//	public String test;
-//some variables
-//	
-//	/**
-//	 * OS route separator
-//	 */
-//	public static final String SEPARATOR = System.getProperty("file.separator");
 
 	/**
 	 * Main constructor.
@@ -37,54 +25,24 @@ public class PTAssistantPlugin extends Plugin {
 	 */
 	public PTAssistantPlugin(PluginInformation info) {
 		super(info);
+		
+		int arrayLength = OsmValidator.getAllAvailableTests().length;
+
 
 		
-//		JOptionPane.showMessageDialog(null, "Download completed", "Question",
-//		        JOptionPane.INFORMATION_MESSAGE);
+		OsmValidator.addTest(PlatformsFirst.class);
+		arrayLength = OsmValidator.getAllAvailableTests().length;
+		JOptionPane.showMessageDialog(null, "after the addition, the array length is " + (new Integer(arrayLength)).toString());
+		
 
-//		createGridManuallyAction = new CreateGridManuallyAction(this);
-//		createGridManuallyMenu = MainMenu.add(Main.main.menu.viewMenu, createGridManuallyAction, false);
-//
-//		createGridAutomaticallyAction = new CreateGridAutomaticallyAction(this);
-//		createGridAutomaticallyMenu = MainMenu.add(Main.main.menu.viewMenu, createGridAutomaticallyAction, false);
-//
-//		splitGridAction = new SplitGridAction();
-//		splitGridMenu = MainMenu.add(Main.main.menu.viewMenu, splitGridAction, false);
-//		
-//		JMenu markGridMenu = new JMenu("Mark grid as");
-//		Main.main.menu.viewMenu.add(markGridMenu);
-//		this.markAsTodoAction = new MarkAsTodoAction();
-//		this.markAsTodoMenu = MainMenu.add(this.markGridMenu, this.markAsTodoAction);
-//		this.markAsDoingAction = new MarkAsDoingAction();
-//		this.markAsDoingMenu = MainMenu.add(this.markGridMenu, this.markAsDoingAction);
-//		this.markAsDoneAction = new MarkAsDoneAction();
-//		this.markAsDoneMenu = MainMenu.add(this.markGridMenu, this.markAsDoneAction);
+//		JOptionPane.showMessageDialog(null, "before route");
+
+		
+		
+
+		
 
 	}
 
-//	@Override
-//	public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {
-//		// does nothing
-//	}
-//	
-//	@Override
-//	public PreferenceSetting getPreferenceSetting() {
-//		// TODO
-//		return null;
-//	}
-//
-//	/**
-//	 * Returns a ImageProvider for the given string or null if in headless mode.
-//	 *
-//	 * @param s
-//	 *            The name of the file where the picture is.
-//	 * @return A ImageProvider object for the given string or null if in
-//	 *         headless mode.
-//	 */
-//	public static ImageProvider getProvider(String s) {
-//		if (Main.main == null) {
-//			return null;
-//		}
-//		return new ImageProvider(s);
-//	}
+
 }
